8352ec3c54c4548ffcd3d3fa2f6f2b38fd92b90c
Johanna Herrmann <johanna.herrmann@mailbox.org>
2025-03-07 14:05:32 +0100

    fix: fixes env and properties output if array permission notation is used

--- lib/command/config.ts
+++ lib/command/config.ts
@@ -25,8 +25,14 @@ const toPropertiesNotation = function (config: Record<string, unknown>, prefix =
     if (key === 'directoryPermissions') {
       const keys = Object.keys(config.directoryPermissions ?? {});
       const values = Object.values(config.directoryPermissions ?? {});
+      const fixedValues = values.map((value) => {
+        if (typeof value === 'string') {
+          return value;
+        }
+        return value.join(':');
+      });
       entries.push(`${prefix}${key}.directories=${keys.join(',')}`);
-      entries.push(`${prefix}${key}.permissions=${values.join(',')}`);
+      entries.push(`${prefix}${key}.permissions=${fixedValues.join(',')}`);
       return;
     }
     if (typeof value === 'object' && 'push' in value) {
